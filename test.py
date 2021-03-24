796d11feb26d169d0db7ddec1ae3a1706a9edad1
leonardo <leocs.oficial@gmail.com>
2021-03-23 23:14:28 -0300

    travis

---
+++ test.py
@@ -0,0 +1,17 @@
+from calc import *
+
+
+def test_somar():
+    assert somar(4, 2) == 6
+
+
+def test_subtrair():
+    assert subtrair(4, 2) == 2
+
+
+def test_multiplicar():
+    assert multiplicar(4, 2) == 8
+
+
+def test_dividir():
+    assert dividir(4, 2) == 2
